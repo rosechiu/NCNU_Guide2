1e5c8b7f1c9f4c54bd0988105c365298e8941b8a
rosechiu <rose103321016@gmail.com>
2017-03-06 22:40:31 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/csie/chiu/ncnu_guide/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.com.csie.chiu.ncnu_guide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
